d74ce80848298c9fd7b37efcc868de7960724b58
Sofía González <sofia_1.6@hotmail.com>
2024-01-01 22:59:16 -0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+
+let myInterval;
 
 function App() {
+  const [minutes, setMinutes] = useState(1);
+  const [seconds, setSeconds] = useState(0);
+  const [selectedLength, setSelectedLength] = useState(true); // Estado de trabajo o descanso
+  const [isActive, setIsActive] = useState(false); // Estado del temporizador: activo o detenido
+
+
+  // const decrementSeconds = () => {
+  //   setSeconds(seconds - 1)
+//   if(seconds === 0){
+  //     setMinutes(minutes - 1)
+  //   }
+  // }
+
+  const decrementSeconds = () => {
+    setSeconds((prevSeconds) => {
+      if (prevSeconds === 0) {
+        setMinutes((prevMinutes) => {
+          if (prevMinutes === 0) {
+            // Aquí podrías manejar el final del temporizador si es necesario
+            return 0; // Por ejemplo, detener el temporizador
+          }
+          return prevMinutes - 1; // Reducir los minutos
+        });
+        return 59; // Reiniciar los segundos a 59
+      } else {
+        return prevSeconds - 1; // Reducir los segundos
+      }
+    });
+  };
+  
+  const handleStartTimer = () => {
+      if (!isActive) {
+        myInterval = setInterval(decrementSeconds, 1000);
+        console.log(myInterval)
+        setIsActive(true);
+      }
+
+  };
+
+  
+  const handlePauseTimer = () => {
+    console.log(myInterval)
+    if (isActive) {
+      clearInterval(myInterval);
+      setIsActive(false);
+    }
+  };
+
+  const handlePomodoro = () => {
+    setMinutes(25);
+    setSeconds(0);
+  };
+
+  const handleBreak = () => {
+    setMinutes(5);
+    setSeconds(0);
+  };
+
+  const handleLongBreak = () => {
+    setMinutes(15);
+    setSeconds(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <menu>
+          <h1>Pomodoro Timer</h1>
+          <button onClick={handlePomodoro}>Pomodoro</button>
+          <button onClick={handleBreak}>Break</button>
+          <button onClick={handleLongBreak}>Long Break</button>
+        </menu>
+        <div>
+          <p>
+            {minutes.toString().padStart(2, "0")}:
+            {seconds.toString().padStart(2, "0")}
+          </p>
+        </div>
+      </section>
+
+      <div>
+        <h6>Controles</h6>
+        <button onClick={handleStartTimer}>Start</button>
+        <button onClick={handlePauseTimer}>Pause</button>
+        <button>Restart</button>
+      </div>
     </div>
   );
 }
